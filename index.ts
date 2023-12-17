d47b90a45be6e56bc23217665070a951e724dab3
chidiebereojingwa <ojingwaoji@gmail.com>
2023-12-17 21:29:09 +0000

    tsc

--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
+import express from 'express'
+
 // create a server
+const app = express()
 
 
+app.get('/', (req, res) => {
+    res.send('<h1> Hello Governor</h1>')
+})
 
 
 
 // listen to some port
+app.listen(8000, () => {
+    console.log("listening")
+})
