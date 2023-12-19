b9ac11877d064530c7bdfced1404f0bda81b95bb
chidiebereojingwa <ojingwaoji@gmail.com>
2023-12-19 06:31:37 +0000

    Add note schema

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-import express from 'express'
-import './db'
+import express from "express";
+import "./db";
 
+// create a server
+const app = express();
+
+// this will parse post request coming from fetch.post() method
+app.use(express.json());
+
+// this will parse post request coming from html form
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/", (req, res) => {
+  // here we need data so that we can create new note/todo
+  console.log(req.body);
+  res.json({ message: "I am listening!" });
+});
+
+app.post("/create", (req, res) => {
+  // here we need data so that we can create new note/todo
+  console.log(req.body);
+  res.json({ message: "I am listening to create!" });
+});
+
+// listen to some port
+app.listen(8000, () => {
+  console.log("listening");
+});
